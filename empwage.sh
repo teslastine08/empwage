dde07ace9b6b567dc7a81b53a4f72a5cca4d3446
Ankan Sinha <ankansinha1@gmail.com>
2020-06-25 11:17:57 +0530

    Update empwage.sh

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "welcome to employee wage computation"
+function main(){ 
+EMP_RATE_PER_HOUR=100
+monthlywage=0
+i=0
+j=0
+for(( i=0;i<20;i++ ))
+do
+randomPresent=$((RANDOM%3))
